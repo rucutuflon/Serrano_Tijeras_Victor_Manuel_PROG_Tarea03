c774e0391b3379b8c4422f1b52b532ab1990a52d
rucut <rucut@DESKTOP-NBA2QKJ>
2018-11-15 17:28:42 +0100

    Commit de la clase Posicion con los metodos get y set

--- Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Posicion.java
+++ Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Posicion.java
@@ -13,4 +13,42 @@ public class Posicion {
     
     private int fila;
     private char columna;
+    
+    /**
+    * Método que devuelve el valor de la propiedad fila.
+    * @return el número de fila.
+    */
+   public int getFila() {
+           return fila;
+   }
+
+   /**
+    * Método que modifica el valor de propiedad fila.
+    * @param fila, número de fila, si no es correcta lanza la excepción.
+    */
+   public void setFila(int fila) {
+           if (fila < 1 || fila > 8) {
+                   throw new IllegalArgumentException("Fila no valida");
+           } else
+                   this.fila = fila;
+   }
+
+   /**
+    * Método que devuelve la propiedad columna.
+    * @return la letra de la columna.
+    */
+   public char getcolumna() {
+           return columna;
+   }
+
+   /**
+    * Método que modifica el valor de la propiedad columna.
+    * @param columna letra con la columna, si no es correcta lanza la excepción.
+    */
+   public void setcolumna(char columna) {
+           if (columna < 'a' || columna > 'h') {
+                   throw new IllegalArgumentException("Columna no valida");
+           } else
+                   this.columna = columna;
+   }
 }
