8613340624faf2cfd13e6d624bd7858228832c6a
rucut <rucut@DESKTOP-NBA2QKJ>
2018-11-15 17:09:39 +0100

    Commit de la clase Direccion

---
+++ Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Direccion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author victor
+ */
+public enum Direccion {
+    
+    ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, 
+    ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO;
+}
