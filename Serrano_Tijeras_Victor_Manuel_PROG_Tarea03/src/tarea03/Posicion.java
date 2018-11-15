24d9ceadffe11a4569b6a8a1a183bc3e13cea0c4
rucut <rucut@DESKTOP-NBA2QKJ>
2018-11-15 17:34:02 +0100

    Commit de la clase Posicion con el metodo constructor

--- Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Posicion.java
+++ Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Posicion.java
@@ -14,41 +14,59 @@ public class Posicion {
     private int fila;
     private char columna;
     
+    
+    /**
+    * Método constructor que inicializa las propiedades de la clase.
+    * 
+    * @param fila, número de fila, si no es correcta lanza la excepción.
+    * @param columna, letra de la columna, si no es correcta lanza la excepción.
+    */
+    public Posicion(int fila, char columna) {
+        if (fila < 1 || fila > 8) {
+                throw new IllegalArgumentException("Fila no valida");
+        } else
+                this.fila = fila;
+
+        if (columna < 'a' || columna > 'h') {
+                throw new IllegalArgumentException("Columna no valida");
+        } else
+                this.columna = columna;
+    }
     /**
     * Método que devuelve el valor de la propiedad fila.
     * @return el número de fila.
     */
-   public int getFila() {
-           return fila;
-   }
+    public int getFila() {
+        return fila;
+    }
 
-   /**
-    * Método que modifica el valor de propiedad fila.
-    * @param fila, número de fila, si no es correcta lanza la excepción.
-    */
-   public void setFila(int fila) {
-           if (fila < 1 || fila > 8) {
-                   throw new IllegalArgumentException("Fila no valida");
-           } else
-                   this.fila = fila;
-   }
+    /**
+     * Método que modifica el valor de propiedad fila.
+     * @param fila, número de fila, si no es correcta lanza la excepción.
+     */
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+                throw new IllegalArgumentException("Fila no valida");
+        } else
+                this.fila = fila;
+    }
 
    /**
     * Método que devuelve la propiedad columna.
     * @return la letra de la columna.
     */
-   public char getcolumna() {
-           return columna;
-   }
+    public char getcolumna() {
+        return columna;
+    }
 
    /**
     * Método que modifica el valor de la propiedad columna.
     * @param columna letra con la columna, si no es correcta lanza la excepción.
     */
-   public void setcolumna(char columna) {
-           if (columna < 'a' || columna > 'h') {
-                   throw new IllegalArgumentException("Columna no valida");
-           } else
-                   this.columna = columna;
-   }
+    public void setcolumna(char columna) {
+        if (columna < 'a' || columna > 'h') {
+                throw new IllegalArgumentException("Columna no valida");
+        } else
+                this.columna = columna;
+    }
 }
