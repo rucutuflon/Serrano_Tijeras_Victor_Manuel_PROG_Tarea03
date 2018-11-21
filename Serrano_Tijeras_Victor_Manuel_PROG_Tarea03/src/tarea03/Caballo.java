aab162d9364de4f6251ddb2ccf450dd2349905aa
rucut <rucut@DESKTOP-NBA2QKJ>
2018-11-21 09:46:12 +0100

    Commit de la clase Caballo con los metodos get y set

--- Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Caballo.java
+++ Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Caballo.java
@@ -13,4 +13,22 @@ public class Caballo {
     
     private Color color;
     private Posicion posicion;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+    
+    
 }
