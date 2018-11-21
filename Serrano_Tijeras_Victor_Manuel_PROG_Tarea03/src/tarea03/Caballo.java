d3e831b43484272f769231ffb771b1504ecf76f7
rucut <rucut@DESKTOP-NBA2QKJ>
2018-11-21 09:44:06 +0100

    Commit de la clase Caballo

---
+++ Serrano_Tijeras_Victor_Manuel_PROG_Tarea03/src/tarea03/Caballo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author victor
+ */
+public class Caballo {
+    
+    private Color color;
+    private Posicion posicion;
+}
